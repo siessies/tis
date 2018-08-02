17f698a5abfce7bd78a0ac9c156c2fd04571ca9e
GTWY-WIN10\consucorp <siescam@yahoo.com>
2018-08-01 23:48:45 -0500

    Export treeTypes in index.ts & ion-select required

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,3 +2,5 @@ export { Api } from './api/api';
 export { Items } from '../mocks/providers/items';
 export { Settings } from './settings/settings';
 export { User } from './user/user';
+
+export { TreeTypes } from './treeTypes/treeTypes';
